2a2b8e10660996bf7db11a1344ec0ffd1e4445c0
sahil9032 <bhuvasahil@gmail.com>
2021-02-25 22:49:17 +0530

    My setup

---
+++ Sahil/cmake-build-debug/CMakeFiles/Sahil.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sahil.dir/main.cpp.obj"
+  "Sahil.exe"
+  "Sahil.exe.manifest"
+  "Sahil.pdb"
+  "libSahil.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sahil.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
